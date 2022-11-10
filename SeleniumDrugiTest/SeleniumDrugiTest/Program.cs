acbee57391b30050e41017fefb619a419e2267e1
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-10 21:09:04 +0100

    init

---
+++ SeleniumDrugiTest/SeleniumDrugiTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDrugiTest
+{
+    public class Program
+    {
+        IWebDriver drive = new ChromeDriver();
+        static void Main(string[] args)
+        {
+           
+          
+
+        }
+        [SetUp]
+        public void Inicijalizacija()
+        {
+            drive.Navigate().GoToUrl("https://www.saucedemo.com/");
+            drive.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            IWebElement userName = drive.FindElement(By.Id("user-name"));
+            userName.SendKeys("standard_user");
+
+            IWebElement password = drive.FindElement(By.Id("password"));
+            password.SendKeys("login-button");
+
+            IWebElement loginButton = drive.FindElement(By.Id("login-button"));
+            loginButton.Submit();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+
+            drive.Quit();
+        }
+    }
+}
---
+++ SeleniumPrviTest/SeleniumPrviTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumPrviTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Selenium");
+            
+            driver.Quit();
+
+
+         
+
+
+
+        }
+    }
+}
